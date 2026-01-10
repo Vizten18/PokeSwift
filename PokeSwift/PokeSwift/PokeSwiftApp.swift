9aeb009fe5a813156397c40b99623149ce534cb5
vizten18 <vizten18@hotmail.com>
2026-01-10 18:19:32 -0400

    Initializing project

---
+++ PokeSwift/PokeSwift/PokeSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeSwiftApp.swift
+//  PokeSwift
+//
+//  Created by Vicente Herrera on 10/1/26.
+//
+
+import SwiftUI
+
+@main
+struct PokeSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
